4aef67ac4c52d929af2fa7c74fe2c803c0d308b4
Antonios Karagiannis <antokarag@gmail.com>
2020-04-12 20:27:41 -0600

    refactor(apollo server): move resolvers

--- resources/publicGraphql/apolloServer/apolloServer.ts
+++ resources/publicGraphql/apolloServer/apolloServer.ts
@@ -1,43 +1,10 @@
 import { ApolloServer } from 'apollo-server-lambda';
-import { IResolvers, IEnumResolver } from 'graphql-tools';
-import { DynamoDB } from 'aws-sdk';
 import { awsConfig } from 'resources/shared/awsConfig';
-import {
-  context,
-  TContext,
-} from 'resources/publicGraphql/apolloServer/context';
+import { context } from 'resources/publicGraphql/apolloServer/context';
 import { typeDefs } from 'resources/publicGraphql/apolloServer/typeDefs';
+import { resolvers } from 'resources/publicGraphql/apolloServer/resolvers';
 
 awsConfig.update({});
-
-// Provide resolver functions for your schema fields
-const resolvers: IResolvers = {
-  Query: {
-    hello: async (
-      parent: IEnumResolver,
-      args: undefined,
-      ctx: TContext,
-    ): Promise<String | undefined> => {
-      const params: DynamoDB.Types.GetItemInput = {
-        Key: {
-          itemKey: {
-            S: 'test-key-1',
-          },
-        },
-        TableName: 'items',
-      };
-
-      try {
-        const data: DynamoDB.GetItemOutput = await ctx.dynamoDB
-          .getItem(params)
-          .promise();
-        return Promise.resolve(data?.Item?.value.S);
-      } catch (e) {
-        return Promise.resolve(e.message);
-      }
-    },
-  },
-};
 
 const apolloServer: ApolloServer = new ApolloServer({
   typeDefs,
