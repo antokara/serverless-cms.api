f61d844dac0d834df2546bd68daccf907ea312d7
Antonios Karagiannis <antokarag@gmail.com>
2020-05-25 12:45:09 -0600

    feat(cloudfront dev): add lambda@edge format

--- localdev/cloudfront/index.ts
+++ localdev/cloudfront/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
 import { S3 } from 'aws-sdk';
+import {
+  CloudFrontResponseEvent,
+  Context,
+  Callback,
+  CloudFrontResultResponse,
+} from 'aws-lambda';
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
 import { s3ClientConfig } from './s3ClientConfig';
+// TODO: add path aliases for this project
+// TODO: add linter
+// TODO: add webpack
+// TODO: add babel-ts
+// TODO: add ts fork checker
+import { handler as publicImagesHandler } from '../../resources/publicImages/app';
 
 // initialize express app
 const app: express.Application = express();
@@ -19,19 +31,90 @@ const s3: S3 = new S3(s3ClientConfig);
  *  it passes the request to the lambda@edge. Since this does not really have a cache,
  *  it will check the S3 bucket with the transformed images in its place.
  *
+ * format of transformed image file names:
+ *    hash__w500_h250_fp50x10_fbw_flipxy.jpeg
+ *    hash  a random string or user generator (must not include __ two consecutive underscores)
+ *        __  hash / parameters separator (__ two consecutive underscores)
+ *          w500  width 500px
+ *              _  parameter separator
+ *               h250  height 250px
+ *                    fp50x10  focal point 50%x10%
+ *                            fbw  filter: black/white
+ *                                flip x/y
  */
-app.get('/', async (req: express.Request, res: express.Response) => {
-  return s3
-    .getObject({
-      Bucket: 'serverless-cms.files.dev',
-      Key: String(req.query.id),
-    })
+app.get('/*', async (req: express.Request, res: express.Response) => {
+  s3.getObject({
+    Bucket: 'serverless-cms.files.dev',
+    Key: String(req.path),
+  })
     .promise()
     .then((image: GetObjectOutput) => {
-      res.send('found');
+      // pass the request normally to the relative S3 bucket
+      res.statusCode = 200;
+      res.setHeader('Content-Type', image.ContentType);
+      res.send(image.Body);
     })
     .catch(() => {
-      res.send('not found');
+      // not found, invoke the publicImages lambda@edge to transform the image and then continue
+      // the request to the transformed S3 bucket;
+      const event: CloudFrontResponseEvent = {
+        Records: [
+          {
+            cf: {
+              config: {
+                distributionDomainName: '',
+                distributionId: '',
+                eventType: 'origin-request',
+                requestId: '',
+              },
+              request: {
+                method: 'GET',
+                clientIp: '',
+                uri: '',
+                querystring: '',
+                headers: {},
+              },
+              response: {
+                status: '200',
+                statusDescription: 'ok',
+                headers: {},
+              },
+            },
+          },
+        ],
+      };
+      const context: Context = {
+        callbackWaitsForEmptyEventLoop: false,
+        functionName: '',
+        functionVersion: '',
+        invokedFunctionArn: '',
+        memoryLimitInMB: '',
+        awsRequestId: '',
+        logGroupName: '',
+        logStreamName: '',
+        getRemainingTimeInMillis: () => 0,
+        done: () => {},
+        fail: () => {},
+        succeed: () => {},
+      };
+      const callback: Callback = () => {};
+      const result: Promise<
+        CloudFrontResultResponse
+      > | void = publicImagesHandler(event, context, callback);
+      if (result) {
+        result
+          .then((result: CloudFrontResultResponse): void => {
+            res.statusCode = 200;
+            res.send(result.body);
+          })
+          .catch(() => {
+            res.statusCode = 500;
+            res.send('Failed to transform');
+          });
+      } else {
+        res.statusCode = 500;
+        res.send('No transform response was returned');
+      }
     });
 });
 
--- resources/publicImages/app.ts
+++ resources/publicImages/app.ts
@@ -1,23 +1,37 @@
 import {
-  APIGatewayProxyHandler,
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
+  CloudFrontResponseHandler,
+  CloudFrontResponseEvent,
+  CloudFrontResultResponse,
 } from 'aws-lambda';
 import { S3 } from 'aws-sdk';
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
-import { s3ClientConfig } from 'resources/shared/s3ClientConfig';
+// TODO: fix path alias because it gets invoked by localdev/cloudfront
+import { s3ClientConfig } from '../../resources/shared/s3ClientConfig';
 import Sharp, { OutputInfo } from 'sharp';
 
-const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
+/**
+ * format of transformed image file names:
+ *    hash__w500_h250_fp50x10_fbw_flipxy.jpeg
+ *    hash  a random string or user generator (must not include __ two consecutive underscores)
+ *        __  hash / parameters separator (__ two consecutive underscores)
+ *          w500  width 500px
+ *              _  parameter separator
+ *               h250  height 250px
+ *                    fp50x10  focal point 50%x10%
+ *                            fbw  filter: black/white
+ *                                flip x/y
+ * @param event
+ */
+const handler: CloudFrontResponseHandler = async (
+  event: CloudFrontResponseEvent,
+): Promise<CloudFrontResultResponse> => {
   const s3: S3 = new S3(s3ClientConfig);
   if (process.env.AWS_S3_BUCKET) {
     try {
       const originalImage: GetObjectOutput = await s3
         .getObject({
           Bucket: process.env.AWS_S3_BUCKET,
-          Key: event.queryStringParameters?.id ?? '',
+          Key: event.Records[0].cf.request?.uri ?? '',
         })
         .promise();
       if (typeof originalImage.Body !== 'undefined') {
@@ -39,24 +53,18 @@ const handler: APIGatewayProxyHandler = async (
           .promise();
 
         return Promise.resolve({
-          statusCode: 200,
+          status: '200',
           body: 'success',
         });
       }
     } catch (e) {
       // could not getObject, transform or putObject
-      return Promise.resolve({
-        statusCode: 500,
-        body: 'failure',
-      });
+      return Promise.reject(new Error('cloud not find master image'));
     }
   }
 
   // no s3 bucket was defined
-  return Promise.resolve({
-    statusCode: 500,
-    body: 'failure',
-  });
+  return Promise.reject(new Error('no S3 was defined'));
 };
 
 export { handler };
